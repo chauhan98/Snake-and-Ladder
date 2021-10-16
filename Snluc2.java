ba3880ca04d00d8a1540e5716fd8ceb79ba79bf2
chauhan98 <cshashank.2598@gmail.com>
2021-10-16 14:57:44 +0530

    2nd user case

---
+++ Snluc2.java
@@ -0,0 +1,8 @@
+public class Snluc2 {
+	public static void main (String[] args){
+        System.out.println(" Let's play Snake and Ladder");
+	int dice = (int) Math.floor((Math.random()*6) +1);
+	System.out.println(dice);
+
+ }
+}
